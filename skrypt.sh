fc91b6e4d24436c3ec5697686536b82f68318ef9
Cezary <dsw55289@student.dsw.edu.pl>
2024-05-13 09:36:33 +0200

    logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# Check if the script is called with the "--date" argument
 if [ "$1" == "--date" ]; then
     date +"%d-%m-%Y %H:%M:%S"
+elif [ "$1" == "--logs" ]; then
+    if [[ "$2" > 0 ]]; then
+        for ((i = 1; i <= $2; i++)); do
+        	touch "log$i.txt"
+    	done
+    else
+	    for ((i = 1; i < 101; i++)); do
+		touch "log$i.txt"
+	    done
+    fi
 else
     echo "przyjmowane flagi:"
     echo "$0 --date"
+    echo "$0 --logs"
+    echo "$0 --logs x (gdzie x to dodatnia liczba calowita"
 fi
 
